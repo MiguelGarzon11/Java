02fefedd6265e2ca6c90808ff3c9c220bb826c58
Miguel Angel <miguelangelgarb@gmail.com>
2025-08-13 19:59:29 -0500

    Principios de Java

---
+++ POO2/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        int numero1 = 10, numero2 = 5, suma = numero1 + numero2; 
+
+        System.out.println("El resultado es: "+suma);
+    }
+}
